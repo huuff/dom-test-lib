429ecd5a6144f2e2b27e795ea1436fe6e20bc242
Haf <haf@protonmail.ch>
2024-10-15 23:43:36 +0200

    feat: select_option panics when it can't find anything

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use wasm_bindgen::JsCast;
+
 /// Awaits a small amount of time so hopefully effects will have run.
 ///
 /// I think this isn't necessary for leptos 0.7 since it exposes
@@ -24,6 +26,23 @@ impl web_sys::HtmlSelectElement {
     ///
     /// panics if the option doesn't exist
     fn select_option(&self, val: &str) {
+        // TODO a helper function that converts a NodeList to an element vector
+        let opts = self.query_selector_all("option").unwrap();
+        let mut found = false;
+        for i in 0..opts.length() {
+            let element = opts
+                .get(i)
+                .unwrap()
+                .unchecked_into::<web_sys::HtmlOptionElement>();
+            if element.value() == val {
+                found = true;
+            }
+        }
+
+        if !found {
+            panic!("option with value `{val}` not found");
+        }
+
         self.set_value(val);
         let event_init = web_sys::EventInit::new();
         event_init.set_bubbles(true);
@@ -69,5 +88,30 @@ mod tests {
         select.select_option("2");
 
         assert_eq!(select.value(), "2");
+    }
+
+    #[should_panic]
+    #[wasm_bindgen_test]
+    fn panics_on_not_found() {
+        let test_wrapper = mount_test_wrapper();
+
+        mount_to(test_wrapper.clone().unchecked_into(), || {
+            view! {
+                <select>
+                    <option value="">none</option>
+                    <option value="1">first</option>
+                    <option value="2">second</option>
+                    <option value="3">third</option>
+                </select>
+            }
+        });
+
+        let select = test_wrapper
+            .query_selector("select")
+            .unwrap()
+            .unwrap()
+            .unchecked_into::<HtmlSelectElement>();
+
+        select.select_option("4");
     }
 }
