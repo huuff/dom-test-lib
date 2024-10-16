be52147b5971bb06d30a8cd36eacd263cce9b236
Haf <haf@protonmail.ch>
2024-10-16 23:42:16 +0200

    test: some tests for the dom macro

--- src/dom_macro.rs
+++ src/dom_macro.rs
@@ -9,9 +9,45 @@ macro_rules! dom {
         }
     };
     ($wrapper:ident, with $selector:literal assert exists) => {
-        assert!($wrapper.query_selector($selector).unwrap().is_some())
+        assert!($wrapper.query_selector($selector).unwrap().is_some());
     };
     ($wrapper:ident, with $selector:literal assert not exists) => {
-        assert!($wrapper.query_selector($selector).unwrap().is_none())
+        assert!($wrapper.query_selector($selector).unwrap().is_none());
     };
+    ($wrapper:ident, with $selector:literal assert text is $text:literal) => {
+        assert_eq!(dom!($wrapper, with $selector).text_content().unwrap(), $text);
+    };
+    ($wrapper:ident, with $selector:literal assert text contains $text:literal) => {
+        assert!(dom!($wrapper, with $selector).text_content().unwrap().contains($text));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use leptos::view;
+    use wasm_bindgen_test::*;
+
+    use crate::mount_test;
+
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[wasm_bindgen_test]
+    fn assert_exists() {
+        let test_wrapper = mount_test(|| {
+            view! { <span id="existent">this exists</span> }
+        });
+
+        dom!(test_wrapper, with "#existent" assert exists);
+        dom!(test_wrapper, with "#non-existent" assert not exists);
+    }
+
+    #[wasm_bindgen_test]
+    fn assert_text() {
+        let test_wrapper = mount_test(|| {
+            view! { <span id="existent">this exists</span> }
+        });
+
+        dom!(test_wrapper, with "#existent" assert text is "this exists");
+        dom!(test_wrapper, with "#existent" assert text contains "exists");
+    }
 }
